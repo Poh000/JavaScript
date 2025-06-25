84eaed149b1f0596d2e91a48d9ddf2550f936a53
Poh000 <bierman.noa3@gmail.com>
2025-06-25 12:26:52 +0200

    Exo liste pokemon fond jaune

---
+++ API Exo1/index.js
@@ -0,0 +1,17 @@
+async function listePokemon(){
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon');
+
+    const result = await data.json();
+    const poke = document.getElementById("liste");
+    tab = result.results;
+    for (let i = 0; i< 20; i++){
+        const li = document.createElement("ul");
+        li.textContent = result.results[i].name;
+        liste.appendChild(li);
+        console.log(result.results[i].name);
+    }
+    liste.style.background=("yellow");
+    liste.style.width=("200"+"px");
+}
+
+console.log(listePokemon())
